32ec17db5e5cbdf684c6015713ae91305ebf4436
Hallae <mr.tabby@hotmail.com>
2024-03-31 05:37:17 +0300

    Update: id and author properties

--- Application.cs
+++ Application.cs
@@ -5,8 +5,9 @@ namespace myApi
     public class Application
     {
         [Key] // This attribute specifies that the Id property is the primary key
-        public Guid id { get; set; } = "9c53ea53-a88d-4367-ad8a-281738690412";// Make the property settable and initialize with a new GUID
-        public string author { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        // Make the property settable and initialize with a new GUID
+        public Guid author { get; set; } = Guid.NewGuid();
         public string activity { get; set; }
         public string name { get; set; }
         public string description { get; set; }
