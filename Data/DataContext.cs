dc06a2d2f9df8bbaf8838812ab0291d526018d4d
Hallae <mr.tabby@hotmail.com>
2024-05-11 22:32:01 +0300

    Update: Added unit test 1#  for GetAllapplication method

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -7,15 +7,22 @@ namespace myApi.Data
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Application> Application { get; set; }
-      
 
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
+         {
+             modelBuilder.Entity<Application>()
+                 .HasKey(n => n.id);
+         }
+
+        ///<summary>
+        //parameterless dataContext for unit testing
+        ///</summary>
+        public DataContext() : base(new DbContextOptions<DataContext>())
         {
-            modelBuilder.Entity<Application>()
-                .HasKey(n => n.id);
         }
     }
+  
 
-   
 
 }
---
+++ Unit tests/GetallApplicationsTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using myApi.Controllers;
+using myApi.Data;
+using myApi.Interfaces;
+using myApi.Models;
+using Xunit;
+
+namespace myApi.Unit_tests
+{
+    public class GetallApplicationsTest
+    {
+        [Fact]
+        public async Task Get_ReturnsAllApplications()
+        {
+            var mockContext = new Mock<DataContext>();
+            var mockRepo = new Mock<IContextApplication>();
+            var controller = new ApplicationController(mockContext.Object, mockRepo.Object, new GuidGenerator());
+
+
+
+            var result = await controller.Get();
+
+            var actionResult = Assert.IsType<ActionResult<List<Application>>>(result);
+            var applications = actionResult.Value;
+         
+        }
+    }
+}
